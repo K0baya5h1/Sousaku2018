7397af97eba43c63423a8dddd6c77242d80669b7
k0bayash1 <15220@fukushima.kosen-ac.jp>
2018-12-12 15:40:53 +0900

    単方向振り下げ判定完了

--- app/src/main/java/com/example/sousaku/sousaku2018/MainActivity.kt
+++ app/src/main/java/com/example/sousaku/sousaku2018/MainActivity.kt
@@ -30,30 +30,53 @@ class MainActivity : AppCompatActivity() ,SensorEventListener,LocationListener{
     }
 
 
-
+    var c:Int=0
+    var flag:Int=0
+    var acflag:Int=0
+    var orflag:Int=0
+    var orflag2:Int=0
+    var updown:Double=0.0
     override fun onSensorChanged(event: SensorEvent?) {
-        /*var phi:Double
-        var q:Double=0.0
-
-            q=event.values[1].toDouble()
-            textView.text="${q.toString()}"
-            phi=Math.atan(q.toDouble())
-            textView2.text=(phi.toString())
-            //地磁気やってみたった
-*/
         if(event==null) return
-        textView.text=("角度:"+event.values[0].toInt().toString())
-        textView2.text=("ピッチ:"+event.values[1].toInt().toString())
-        textView3.text=("ロール:"+event.values[2].toInt().toString())
+        if(event.sensor.type==Sensor.TYPE_ORIENTATION) {
+            if(event.values[1]>=-15.0){  //ピッチ判定
+                orflag=1
+            }else{
+                orflag=0
+            }
+            if(event.values[1]-updown>0.0){ //振り上げカウント禁止(現在のピッチ-直前のピッチで判定)
+                orflag2=1
+            }else{
+                orflag2=0
+            }
+            updown=event.values[1].toDouble()
+        }
+        if(event.sensor.type==Sensor.TYPE_ACCELEROMETER){
+            if(event.values[2]>30.0){ //加速度で鳴らすか判定
+                acflag=1
+            }
+            if(event.values[2]<10.0){ //チャタリング的な奴の防止策(役に立ってるかはわからない)
+                acflag=0
+            }
+            if(orflag==1 && flag==0 && acflag==1 && orflag2==1){ //全条件一致で鳴らしましょってところのところ
+                c++
+                flag=1
+            }
+            if(event.values[2]<10.0 && flag==1){ //鳴らし終わって加速度が戻ったら解除しましょってところのところのところ
+                flag=0
+            }
+        }
 
-
+        textView.text=c.toString() //カウント表示
 
     }
 
     override fun onResume() {
         super.onResume()
         val sensorManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val accSensor=sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION)
+        val oriSensor=sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION)
+        val accSensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this,oriSensor,SensorManager.SENSOR_DELAY_GAME)
         sensorManager.registerListener(this,accSensor,SensorManager.SENSOR_DELAY_GAME)
     }
 
